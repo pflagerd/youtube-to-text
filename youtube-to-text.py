8bd47083b892b3f437337fa9b305c346b188ec2d
Daniel P Pflager <daniel.pflager@gmail.com>
2025-08-29 15:42:26 -0700

    In case some crazy person tries to use it as a library.

--- youtube-to-text.py
+++ youtube-to-text.py
@@ -7,27 +7,25 @@ import mp3_to_text
 import os
 import sys
 
-from openai import OpenAI
-from pathlib import Path
+if __name__ == "__main__":
+    usage="youtube-to-text.py some-youtube.url"
 
-usage="youtube-to-text.py some-youtube.url"
+    if len(sys.argv) != 2:
+      print(usage)
+      sys.exit(1)
 
-if len(sys.argv) != 2:
-  print(usage)
-  sys.exit(1)
+    # Example usage
+    video_url = sys.argv[1]
+    print("Downloading youtube video from " + video_url)
+    mp4_filename = download_from_youtube.download_mp4(video_url)
 
-# Example usage
-video_url = sys.argv[1]
-print("Downloading youtube video from " + video_url)
-mp4_filename = download_from_youtube.download_mp4(video_url)
+    print("Extracting mp3(s) from " + mp4_filename)
+    mp3_filenames = extract_mp3s_from_youtube_mp4.extract_mp3s(mp4_filename)
 
-print("Extracting mp3(s) from " + mp4_filename)
-mp3_filenames = extract_mp3s_from_youtube_mp4.extract_mp3s(mp4_filename)
-
-if not isinstance(mp3_filenames, list):
-    mp3_to_text.to_text(mp3_filenames)
-else:
-    txt_filenames = []
-    for mp3_filename in mp3_filenames:
-        print("Extracting text from" + mp3_filename)
-        txt_filenames.append(mp3_to_text.to_text(mp3_filename))
+    if not isinstance(mp3_filenames, list):
+        mp3_to_text.to_text(mp3_filenames)
+    else:
+        txt_filenames = []
+        for mp3_filename in mp3_filenames:
+            print("Extracting text from" + mp3_filename)
+            txt_filenames.append(mp3_to_text.to_text(mp3_filename))
